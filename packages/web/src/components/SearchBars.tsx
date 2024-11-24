9a37652c7713cd69712233a32219f9e576075938
zx <67887489+tan-zx@users.noreply.github.com>
2024-11-25 07:56:01 +0800

    fix: various CSS issues

--- packages/web/src/components/SearchBars.tsx
+++ packages/web/src/components/SearchBars.tsx
@@ -114,7 +114,9 @@ export function HomepageSearchBar() {
           onFocus={handleFocus}
           onBlur={handleBlur}
           className={cn(
-            "w-full rounded-full border border-gray-300 py-2 pl-10 pr-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500",
+            "rounded-full pl-10",
+            "border-gray-300",
+            "focus:border-transparent focus:ring-2 focus:ring-blue-500",
             preventInputZoom,
           )}
         />
