6774d814af50ba9cc426206ebaa1fed4338ac1f8
zx <67887489+tan-zx@users.noreply.github.com>
2024-12-04 19:46:58 +0800

    expand eslint gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { includeIgnoreFile } from "@eslint/compat";
 import tseslint from "typescript-eslint";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
 export default [
+  includeIgnoreFile(gitignorePath),
   {
     ignores: [
       ".sst/**/*",
       "**/sst-env.d.ts",
       "**/schema.docs.graphql.d.ts",
       "**/routeTree.gen.ts",
-      "**/dist/**/*",
     ],
   },
   {
