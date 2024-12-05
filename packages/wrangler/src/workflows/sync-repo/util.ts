6f35bf7b905896b5f6dd51b99522b2be7da21229
zx <67887489+tan-zx@users.noreply.github.com>
2024-12-05 09:10:36 +0800

    ParamsRPC

--- packages/wrangler/src/workflows/sync-repo/util.ts
+++ packages/wrangler/src/workflows/sync-repo/util.ts
@@ -1,6 +1,7 @@
-// cannot send WorkflowInstance over RPC, so must write substitute methods
-// be careful: if you send a non-string payload, it will be serialized to string and lose its methods
-export interface WorkflowRPC<T = unknown, E = unknown> {
+// params sent over RPC will be converted to string
+type ParamsRPC = string | { [key: string]: ParamsRPC } | ParamsRPC[];
+// cannot send classes with methods (?) over RPC, so must write substitute methods
+export interface WorkflowRPC<T extends ParamsRPC = ParamsRPC, E = unknown> {
   fetch(request: Request, env?: E): Promise<Response>;
   create(
     options?: Omit<WorkflowInstanceCreateOptions, "params"> & { params?: T },
