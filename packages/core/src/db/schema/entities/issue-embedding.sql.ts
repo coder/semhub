6e0cf155a1a568b0da628ef62af2be0f7b1461ed
Warren <67887489+zxt-tzx@users.noreply.github.com>
2024-12-14 23:19:56 +0800

    refactor: embedding schema part 1 (new table + migration) (#18)
    * wip
    
    * feat: create new table
    
    * migration script
    
    * migration script chunking
    
    * add unique index, modify script to be interruptible
    
    * fix type error
    
    * fix unneccessary offset
    
    ---------
    
    Co-authored-by: zx <67887489+tan-zx@users.noreply.github.com>

---
+++ packages/core/src/db/schema/entities/issue-embedding.sql.ts
@@ -0,0 +1,42 @@
+import { index, pgEnum, pgTable, text, uniqueIndex, vector } from "drizzle-orm/pg-core";
+
+
+
+import { getBaseColumns, timestamptz } from "../base.sql";
+import { issueTable } from "./issue.sql";
+
+
+export const issueEmbeddingSyncStatusEnum = pgEnum(
+  "issue_embedding_sync_status",
+  ["ready", "in_progress", "error"],
+);
+
+export const issueEmbeddingTable = pgTable(
+  "issue_embeddings",
+  {
+    ...getBaseColumns("issue_embeddings"),
+    issueId: text("issue_id")
+      .references(() => issueTable.id)
+      .notNull(),
+    embeddingModel: text("embedding_model"),
+    // max dimension of 2000 if we use HNSW index; see https://github.com/pgvector/pgvector/issues/461
+    // if we use text-embedding-3-large, which has 3072 dimensions, we need to reduce dimensions
+    // see https://platform.openai.com/docs/api-reference/embeddings/create#embeddings-create-dimensions
+    embedding: vector("embedding", { dimensions: 1536 }),
+    embeddingGeneratedAt: timestamptz("embedding_generated_at"),
+    embeddingSyncStatus: issueEmbeddingSyncStatusEnum(
+      "issue_embedding_sync_status",
+    )
+      .notNull()
+      .default("ready"),
+  },
+  (table) => ({
+    issueEmbeddingsIssueIdIdx: uniqueIndex("issue_embeddings_issue_id_idx").on(
+      table.issueId,
+    ),
+    issueEmbeddingsEmbeddingIdx: index("issue_embeddings_embedding_idx").using(
+      "hnsw",
+      table.embedding.op("vector_cosine_ops"),
+    ),
+  }),
+);
