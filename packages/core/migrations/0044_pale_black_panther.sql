e7b640d31e9be63abf47c714009469abf1f50244
zx <67887489+tan-zx@users.noreply.github.com>
2025-02-11 11:28:47 +0800

    migration

---
+++ packages/core/migrations/0044_pale_black_panther.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "issues" ADD COLUMN "overall_summary" text;--> statement-breakpoint
+ALTER TABLE "issues" ADD COLUMN "body_summary" text;--> statement-breakpoint
+ALTER TABLE "issues" ADD COLUMN "comments_summary" text;
